99c735b3cc1a75df3563099bf68c941ea2f50698
Oyims Jesse <121711700+midnite-ops@users.noreply.github.com>
2024-03-20 16:28:16 +0100

    Add files via upload

---
+++ GTN.js
@@ -0,0 +1,51 @@
+const randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+const user = document.querySelector('.js-input');
+let count = 5;
+function check(){
+    let userGuess = user.value;
+    if(count == 0){
+        let restart = prompt(`GAME OVER! Do you want to restart? Yes or No`)
+        if(restart == 'yes'){
+            start()
+        }
+        else{
+            alert('Thanks for playing, Have a nice day')
+        }
+    }
+    else{
+        if(userGuess < randomNumber){
+            count--;
+            if(count == 1  ||  count == 0){
+                alert(`You guessed too low, you have ${count} chance remaining.`);
+            }
+            else{
+                alert(`You guessed too low, you have ${count} chances remaining.`);
+            }
+            user.value = '';
+        }
+        else if(userGuess > randomNumber){
+            count--;
+            if(count == 1  ||  count == 0){
+                alert(`You guessed too low, you have ${count} chance remaining.`);
+            }
+            else{
+                alert(`You guessed too low, you have ${count} chances remaining.`);
+            }
+            user.value = '';
+        }
+        else{
+            let restart = prompt(`You won the game, do you want to restart? Yes or No`)
+            if(restart == 'yes'){
+                start();
+            }
+            else{
+                alert('Thanks for playing, Have a nice day')
+            }
+        }
+    }
+}
+function start(){
+    location.reload();
+}
+
